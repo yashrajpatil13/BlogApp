a90be1ce59b0e471b4f26f4cf6064735b4eede17
Yashraj Patil <yashraj.patil13@gmail.com>
2024-09-20 13:23:08 +0530

    vercel changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,7 @@ export default function Home(){
             <Container>
                 <div className='flex flex-wrap'>
                     {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4 animate-mymove'>
+                        <div key={post.$id} className='p-2 w-1/4'>
                             <PostCard {...post}/>
                         </div>
                     ))}
